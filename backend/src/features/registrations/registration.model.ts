353b37bcdb62c42e0a215f8b2c09ededaf80a19f
John Abraham Reyes <jbreyes24@up.edu.ph>
2026-04-16 10:39:44 +0800

    fix: switch models located to the wrong files

--- backend/src/features/registrations/registration.model.ts
+++ backend/src/features/registrations/registration.model.ts
@@ -1,13 +1,9 @@
-import { VehicleType } from "@/types/vehicle";
+import { RegistrationStatus } from "@/types/registration";
 
-export interface Vehicle {
-  plate_number: string;
-  engine_number: string;
-  chassis_number: string;
-  vehicle_type: VehicleType;
-  make: string;
-  model: string;
-  year: number;
-  color: string;
-  license_number: string; // References to driver
+export interface VehicleRegistration {
+  registration_number: number;
+  registration_status: RegistrationStatus;
+  registration_date: Date;
+  expiration_date: Date;
+  plate_number: string; // References to vehicle
 }
--- backend/src/features/vehicles/vehicle.model.ts
+++ backend/src/features/vehicles/vehicle.model.ts
@@ -1,9 +1,13 @@
-import { RegistrationStatus } from "@/types/registration";
+import { VehicleType } from "@/types/vehicle";
 
-export interface VehicleRegistration {
-  registration_number: number;
-  registration_status: RegistrationStatus;
-  registration_date: Date;
-  expiration_date: Date;
-  plate_number: string; // References to vehicle
+export interface Vehicle {
+  plate_number: string;
+  engine_number: string;
+  chassis_number: string;
+  vehicle_type: VehicleType;
+  make: string;
+  model: string;
+  year: number;
+  color: string;
+  license_number: string; // References to driver
 }
